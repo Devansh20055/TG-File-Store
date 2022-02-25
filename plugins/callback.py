d67501df20dfd1d8f31989dadfa083562a9dda9e
DEVANSH <79394737+Devansh20055@users.noreply.github.com>
2022-02-25 18:08:29 +0530

    Update callback.py

--- plugins/callback.py
+++ plugins/callback.py
@@ -62,7 +62,7 @@ async def about_cb(c, m):
     # about text
     about_text = f"""--**My Details:**--
 
-🤖 𝐌𝐲 𝐍𝐚𝐦𝐞: {bot.mention(style='md')}
+🤖 𝐌𝐲 𝐍𝐚𝐦𝐞: {"""FILE STORE BOT"""(style='md')}
     
 📝 𝐋𝐚𝐧𝐠𝐮𝐚𝐠𝐞: [Python 3](https://www.python.org/)
 
